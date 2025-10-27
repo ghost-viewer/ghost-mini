bf9186f0bff39d4e67b8716b4f571cb91c3ba6f8
ghost-viewer <nickykipruto035@gmail.com>
2025-10-27 15:49:51 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 global.tumb = ["https://files.catbox.moe/pf7ytb.jpg", "https://files.catbox.moe/9wrrby.jpg", "https://files.catbox.moe/n8g2o7.jpg", "https://files.catbox.moe/p4y0sz.jpg", "https://files.catbox.moe/qchzmy.jpg", "https://files.catbox.moe/qld3cd.jpg"]
 
-global.owner = ["94721584279"]  
+global.owner = ["254769677305"]  
 global.saluran = "0943932269217268e+33@newsletter"
 global.idgc = "120363421044739622@g.us" 
 
@@ -24,14 +24,14 @@ global.tokenVercel = "_" //Token vercel mu (search vercel token)
 global.tokenGithub = "_" // Token github mu (search github token)
 global.usnGithub = "_" // Nama akun gtihub mu
 
-global.botname = "ɴᴇɴᴏ-xᴍᴅ༒ᴍɪɴɪ" ///setting nama bot
-global.ownername = 'ᴏᴡɴᴇʀ-ɴɪᴍᴇꜱʜᴋᴀ '
-global.ownerNumber = ["94721584279@s.whatsapp.net"] 
-global.wagc = "https://chat.whatsapp.com/Ekoe6EykUJf0KfQShsBfoN" 
-global.wm = "nimeshka-mihiran。" 
-global.wmbot = "NENO_XMD_MINI" 
+global.botname = "GHOST-MINI" ///setting nama bot
+global.ownername = 'Ghost-viewer '
+global.ownerNumber = ["254769677305@s.whatsapp.net"] 
+global.wagc = "https://whatsapp.com/channel/0029Vb6jxRD7IUYbQfokIu2s" 
+global.wm = "HACKLINKTECH。" 
+global.wmbot = "GHOST-MINI" 
 global.packname = "NENO"
-global.author = "• ᴅᴇᴠ | ɴɪᴍᴇꜱʜᴋᴀ "
+global.author = "• ᴅᴇᴠ | Ghost-viewer "
 global.prefa = ['','!','.','#','&']
 global.typemenu = "button" //button, payment, polling, product
 
